49323e31d7b2683fc6e2ded5b2df39343902e1a9
Emma Bowden <emzie.bowden@gmail.com>
2022-03-17 07:47:00 +0000

    fix Header issues

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,40 +1,9 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./header.scss";
 import logo from "../../assets/logo/instock-logo.svg";
 
 export default function Header() {
-  const currentPath = useLocation().pathname;
-  const warehouseClass = "header__warehouse-link";
-  const inventoryClass = "header__inventory-link";
-
-  const warehouseActive = (currClass) => {
-    if (
-      currentPath ===
-      ("/" || "/warehouses-details" || "/warehouses-edit" || "/warehouses-add")
-    ) {
-      console.log("this is a warehouse url");
-      return currClass + "--active";
-    } else {
-      return currClass;
-    }
-  };
-
-  const inventoryActive = (currClass) => {
-    if (
-      currentPath ===
-      ("/inventory" ||
-        "/inventory-details" ||
-        "/inventory-edit" ||
-        "/inventory-add")
-    ) {
-      console.log("this is an inventory url");
-      return currClass + "--active";
-    } else {
-      return currClass;
-    }
-  };
-
   return (
     <section className="header">
       <section className="header__container">
@@ -42,10 +11,23 @@ export default function Header() {
           <img src={logo} alt="" className="header__logo" />
         </article>
         <article className="header__links">
-          <button className={warehouseActive(warehouseClass)}>
+          <NavLink
+            to="/warehouses"
+            className={(isActive) =>
+              "header__warehouse-link" + (!isActive ? "" : "--active")
+            }
+          >
             Warehouses
-          </button>
-          <button className={inventoryActive(inventoryClass)}>Inventory</button>
+          </NavLink>
+          <NavLink
+            strict
+            to="/inventory"
+            className={(isActive) =>
+              "header__inventory-link" + (!isActive ? "" : "--active")
+            }
+          >
+            Inventory
+          </NavLink>
         </article>
       </section>
     </section>
