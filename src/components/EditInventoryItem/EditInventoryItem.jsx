66b4f002609a907a4499764e27b9414138814e6a
Christian Mitchell <97884217+chrismitchelll@users.noreply.github.com>
2022-03-17 17:01:41 +0000

    Revert "J2N-6"

--- src/components/EditInventoryItem/EditInventoryItem.jsx
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-// import "./edit-inventory-item.scss";
-
-export default class EditInventoryItem extends Component {
-  render() {
-    return <div>EditInventoryItem</div>;
-  }
-}
