030cc519b498d4739b412d65abb07b906b2660a3
Emma Bowden <97846259+embowden@users.noreply.github.com>
2022-03-21 21:57:56 +0000

    delete everything

--- src/components/Footer/Footer.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./footer.scss";
-
-export const Footer = () => {
-  return (
-    <footer className="footer">© InStock Inc. All Rights Reserved.</footer>
-  );
-};
