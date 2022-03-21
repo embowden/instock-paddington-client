0dd704d456db00257879b5fbc201c4686828da68
Ben Thorne <benjaminjamesthorne@gmail.com>
2022-03-21 18:24:40 +0000

    Finish functionality and styling for Edit Inventory Component

--- src/components/EditInventoryItem/EditInventoryContainer.jsx
+++ src/components/EditInventoryItem/EditInventoryContainer.jsx
@@ -119,6 +119,7 @@ export default class EditInventoryItem extends Component {
   handleSubmit = (event) => {
     event.preventDefault();
     this.updateItem(event);
+    this.props.history.push("/inventory");
   };
 
   render() {
@@ -182,7 +183,7 @@ export default class EditInventoryItem extends Component {
                 name="category"
                 value={`${this.state.category}`}
               >
-                <option value="Gealth">Health</option>
+                <option value="Health">Health</option>
                 <option value="Accessories">Accessories</option>
                 <option value="Apparel">Apparel</option>
                 <option value="Gear">Gear</option>
@@ -278,9 +279,9 @@ export default class EditInventoryItem extends Component {
           </div>
 
           <div className="edit-inventory__buttons">
-            <button type="reset" className="edit-inventory__cancel">
-              Cancel
-            </button>
+            <Link className="edit-inventory__cancel-link" to="/inventory">
+              <button className="edit-inventory__cancel">Cancel</button>
+            </Link>
             <button type="submit" className="edit-inventory__add">
               Save
             </button>
